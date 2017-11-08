e76d9e5f8d81bb46df585b92794b8566500bcaeb
albert.cwkuo <albert.cwkuo@gmail.com>
2017-11-08 16:28:53 -0500

    train linear SVM classifier using vl_svmtrain

--- proj/proj5/code/proj5.m
+++ proj/proj5/code/proj5.m
@@ -73,8 +73,10 @@ features_neg = get_random_negative_features( non_face_scn_path, feature_params, 
 % work best e.g. 0.0001, but you can try other values
 
 %YOU CODE classifier training. Make sure the outputs are 'w' and 'b'.
-w = rand((feature_params.template_size / feature_params.hog_cell_size)^2 * 31,1); %placeholder, delete
-b = rand(1); %placeholder, delete
+lambda = 0.0001;
+X=[features_pos; features_neg]';
+Y=[ones(1, size(features_pos,1)), ones(1, size(features_neg,1))];
+[w, b] = vl_svmtrain(X, Y, lambda);
 
 %% step 3. Examine learned classifier
 % You don't need to modify anything in this section. The section first
