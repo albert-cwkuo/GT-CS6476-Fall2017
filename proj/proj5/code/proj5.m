27df97d491ec0aa7c6e4fd436d58ea88200727b9
albert.cwkuo <albert.cwkuo@gmail.com>
2017-11-10 21:38:00 -0500

    set cell_size = 3 and reach 92% accuracy

--- proj/proj5/code/proj5.m
+++ proj/proj5/code/proj5.m
@@ -52,7 +52,7 @@ label_path = fullfile(data_path,'test_scenes/ground_truth_bboxes.txt'); %the gro
 %add other fields to this struct if you want to modify HoG default
 %parameters such as the number of orientations, but that does not help
 %performance in our limited test.
-feature_params = struct('template_size', 36, 'hog_cell_size', 6);
+feature_params = struct('template_size', 36, 'hog_cell_size', 3);
 
 
 %% Step 1. Load positive training crops and random negative examples
@@ -60,7 +60,7 @@ feature_params = struct('template_size', 36, 'hog_cell_size', 6);
 
 features_pos = get_positive_features( train_path_pos, feature_params );
 
-num_negative_examples = 10000; %Higher will work strictly better, but you should start with 10000 for debugging
+num_negative_examples = 100000; %Higher will work strictly better, but you should start with 10000 for debugging
 features_neg = get_random_negative_features( non_face_scn_path, feature_params, num_negative_examples);
 
     
