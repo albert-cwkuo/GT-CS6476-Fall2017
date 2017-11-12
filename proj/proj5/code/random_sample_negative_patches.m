4d74501e111424a00edf00de642b149b56773630
albert.cwkuo <albert.cwkuo@gmail.com>
2017-11-11 19:18:27 -0500

    sample negative samples at random scales.

--- proj/proj5/code/random_sample_negative_patches.m
+++ proj/proj5/code/random_sample_negative_patches.m
@@ -1,9 +1,15 @@
 function I = random_sample_negative_patches(image_files, dim)
-num_images = length(image_files);
 % randomly sample one image
+num_images = length(image_files);
 ind = randi(num_images);
 filename = fullfile(image_files(ind).folder, image_files(ind).name);
 I=imread(filename);
+% randomly scale image
+scale_min = 0.4;
+if scale_min*min(size(I,1), size(I,2)) > dim
+    scale = scale_min+(1-scale_min)*rand();
+    I=imresize(I,scale);
+end
 % randomly crop image
 h=size(I,1);
 w=size(I,2);
